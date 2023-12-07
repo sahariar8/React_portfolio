5c59c781c6f91f41626858415cc8c8d3382d2551
Sahariar <sahariaralam8@gmail.com>
2023-12-07 22:13:19 +0600

    Portfolio start

---
+++ src/layout/LayOut.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LayOut = () => {
+    return (
+        <div>
+            <h1 className='text-purple text-5xl text-center'>I am from layout</h1>
+        </div>
+    );
+};
+
+export default LayOut;
